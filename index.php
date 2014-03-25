1299ab665f0ba1e6dd532588e9aa81f811eee720
raphaelfb <raphaelfbeu@gmail.com>
2014-03-25 17:56:53 -0300

    em construcao

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<h1>Hello Design Cocktail</h1>
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Design Cocktail</title>
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link href='http://fonts.googleapis.com/css?family=Emblema+One|Marko+One' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<content>
+		<div id="tx"><span>Em construção</span></div>
+		<div id="logo"><img src="imgs/logo.png"></div>
+	</content>
+</body>
