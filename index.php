b26ac10207497f5976d8b2dc0024f7b0db798180
raphaelfb <raphaelfbeu@gmail.com>
2014-03-26 18:28:27 -0300

    simple constrution

--- index.php
+++ index.php
@@ -6,21 +6,17 @@
             Design Cocktail
         </title>
         <link href="css/style.css" type="text/css" rel="stylesheet"></link>
-        <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Emblema+One|Marko+One"></link>
     </head>
     <body>
-    	<img src="imgs/fundo-construcao.jpg" id="full-screen">
-
-            <div id="tx" align="center">
-            	<img src="imgs/logo.png"></img>
-            	<br>
+        <div id="center">
+            <div id="logo">
+        	   <img src="imgs/logo.png"></img>
+            </div>
+            <div id="text">
                 <span>
                     Em construção
                 </span>
             </div>
-            <div id="logo">
-                
-            </div>
-
+        </div>
     </body>
 </html>
